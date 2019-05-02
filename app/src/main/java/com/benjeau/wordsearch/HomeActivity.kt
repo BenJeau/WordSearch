6e7efd0222f0d62e98e3a60e3c7674079c8659f7
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-05-02 00:13:42 -0400

    Added sample picture for the profile

--- app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
+++ app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
@@ -2,11 +2,21 @@ package com.benjeau.wordsearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val profileIcon: ImageView = findViewById(R.id.profileIcon)
+
+        Glide.with(this)
+            .load("https://api.adorable.io/avatars/100/sdf")
+            .apply(RequestOptions.circleCropTransform())
+            .into(profileIcon)
     }
 }
