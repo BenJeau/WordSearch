d4e4bde4b2f99e3c0a2d3fe0952706e751ef94e0
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-05-02 00:30:46 -0400

    Simplified home layout and started game layout

--- app/src/main/java/com/benjeau/wordsearch/GameActivity.kt
+++ app/src/main/java/com/benjeau/wordsearch/GameActivity.kt
@@ -2,11 +2,25 @@ package com.benjeau.wordsearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val profileIcon: ImageView = findViewById(R.id.profileIcon)
+
+        Glide.with(this)
+            .load("https://api.adorable.io/avatars/100/sdf")
+            .apply(RequestOptions.circleCropTransform())
+            .into(profileIcon)
+
+        val playGame: ImageButton = findViewById(R.id.homeIcon)
+        playGame.setOnClickListener{finish()}
     }
 }
--- app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
+++ app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
@@ -2,9 +2,8 @@ package com.benjeau.wordsearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import android.widget.Button
+import android.content.Intent
 
 class HomeActivity : AppCompatActivity() {
 
@@ -12,11 +11,10 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val profileIcon: ImageView = findViewById(R.id.profileIcon)
-
-        Glide.with(this)
-            .load("https://api.adorable.io/avatars/100/sdf")
-            .apply(RequestOptions.circleCropTransform())
-            .into(profileIcon)
+        val playGame: Button = findViewById(R.id.playGame)
+        playGame.setOnClickListener{
+            val myIntent = Intent(this, GameActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
