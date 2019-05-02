dcb4bc350a34345d3f783c8e74b63eecbf1e6139
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-05-02 01:53:49 -0400

    Added flexbox layout and started word bank

--- app/src/main/java/com/benjeau/wordsearch/GameActivity.kt
+++ app/src/main/java/com/benjeau/wordsearch/GameActivity.kt
@@ -1,11 +1,23 @@
 package com.benjeau.wordsearch
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.flexbox.FlexboxLayout
+import com.google.android.flexbox.FlexboxLayoutManager
+import android.util.TypedValue
+import android.opengl.ETC1.getHeight
+import android.view.ViewTreeObserver
+
+
+
+
 
 class GameActivity : AppCompatActivity() {
 
@@ -22,5 +34,35 @@ class GameActivity : AppCompatActivity() {
 
         val playGame: ImageButton = findViewById(R.id.homeIcon)
         playGame.setOnClickListener{finish()}
+
+        val wordBankLayout: FlexboxLayout = findViewById(R.id.wordBank)
+
+        val wordBank = arrayListOf("Swift", "ObjectiveC", "Java", "Kotlin", "Variable", "Mobile")
+        val wordBankText = arrayListOf<TextView>()
+
+        val dip = 5f
+        val r = resources
+        val px: Int = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dip,
+            r.displayMetrics
+        ).toInt()
+
+        wordBank.forEach{
+            val text = TextView(this)
+            wordBankText.add(text)
+            wordBankLayout.addView(text)
+            text.text = it
+            text.setTextColor(resources.getColor(R.color.white))
+            text.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+            text.setPadding(px, px, px, px)
+        }
+
+        wordBankLayout.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                wordBankLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                wordBankText.forEach{it.width = wordBankLayout.width/4}
+            }
+        })
     }
 }
--- app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
+++ app/src/main/java/com/benjeau/wordsearch/HomeActivity.kt
@@ -1,6 +1,6 @@
 package com.benjeau.wordsearch
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
